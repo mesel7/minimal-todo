5026c3c01f8bcad07b01eed2797d6b8f565c16dd
mesel7 <cejhans1520@gmail.com>
2025-12-31 03:22:23 +0900

    feat: Header 구현

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,14 @@
+import { Calendar } from "lucide-react";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="py-6">
+      <p className="font-semibold">오늘은</p>
+
+      <h1 className="mt-2 flex items-center gap-2 text-3xl font-semibold font-outfit">
+        <Calendar className="h-8 w-8" aria-hidden="true" />
+        {new Date().toDateString()}
+      </h1>
+    </header>
+  );
 }
